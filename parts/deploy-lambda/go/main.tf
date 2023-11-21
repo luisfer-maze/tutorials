bd401ea3f8c8e1f781ef7ff176cf7afb4566c9a6
Luis Fernando Maldonado Zelada <maldonado.luisfer@gmail.com>
2023-11-21 19:15:31 +0000

    providers

--- parts/deploy-lambda/go/main.tf
+++ parts/deploy-lambda/go/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
     archive = {
       source = "hashicorp/archive"
@@ -9,18 +9,9 @@ terraform {
     null = {
       source = "hashicorp/null"
     }
-  }
-
-  required_version = ">= 1.3.7"
+  }  
 }
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "tutorial-terraform-profile"
-
-  default_tags {
-    tags = {
-      app = "tutorial-terraform"
-    }
-  }
 }
